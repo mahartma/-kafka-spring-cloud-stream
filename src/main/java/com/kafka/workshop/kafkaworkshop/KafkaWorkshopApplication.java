0ab28faf3a43ce4449168171f75c3d6a9c2d0a80
mahartma <max.hartmann@hdi.de>
2022-01-12 15:14:51 +0100

    usage of Kafka Streams

--- src/main/java/com/kafka/workshop/kafkaworkshop/KafkaWorkshopApplication.java
+++ src/main/java/com/kafka/workshop/kafkaworkshop/KafkaWorkshopApplication.java
@@ -1,5 +1,8 @@
 package com.kafka.workshop.kafkaworkshop;
 
+import org.apache.kafka.streams.kstream.KStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,12 +12,17 @@ import java.util.function.Consumer;
 @SpringBootApplication
 public class KafkaWorkshopApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaWorkshopApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(KafkaWorkshopApplication.class, args);
     }
 
     @Bean
-    public Consumer<String> myConsumer() {
-        return (value) -> System.out.println("Consumer Received : " + value);
+    public Consumer<KStream<Object, String>> myConsumer() {
+        return input -> input
+                .groupBy((key, value) -> value)
+                .count()
+                .toStream().foreach((key, value) -> LOG.info("Value: " + key + ", Count: " + value));
     }
 }
