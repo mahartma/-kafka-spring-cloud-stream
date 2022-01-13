6adbf83fcac2095c3c9ce953430cbe47e41db61b
mahartma <max.hartmann@hdi.de>
2022-01-13 15:21:36 +0100

    add testcontainers

--- src/main/java/com/kafka/workshop/kafkaworkshop/KafkaWorkshopApplication.java
+++ src/main/java/com/kafka/workshop/kafkaworkshop/KafkaWorkshopApplication.java
@@ -1,6 +1,5 @@
 package com.kafka.workshop.kafkaworkshop;
 
-import org.apache.kafka.streams.kstream.KStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
--- src/test/java/com/kafka/workshop/kafkaworkshop/KafkaWorkshopApplicationTests.java
+++ src/test/java/com/kafka/workshop/kafkaworkshop/KafkaWorkshopApplicationTests.java
@@ -1,10 +1,29 @@
 package com.kafka.workshop.kafkaworkshop;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = KafkaWorkshopApplication.class)
+@Testcontainers
 class KafkaWorkshopApplicationTests {
+
+    @Container
+    public static KafkaContainer kafka =
+            new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.4.3"));
+
+    @DynamicPropertySource
+    private static void dynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.cloud.stream.kafka.binder.brokers", () -> kafka.getBootstrapServers());
+    }
 
     @Test
     void contextLoads() {
